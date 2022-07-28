ad4b7e5577f6d05ed4e6baf154457f096ca96ef4
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-28 11:22:22 +0600

    Bugs:fixed song api endpoint

--- app.ts
+++ app.ts
@@ -41,6 +41,7 @@ import user from "./routes/user";
 app.use("/api/v1/auth", auth)
 app.use("/api/v1/artist", artist)
 app.use("/api/v1/album", album)
+app.use("/api/v1/song", song)
 app.use("/api/v1/admin", user)
 
 // Middleware Error Handler
