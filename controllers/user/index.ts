a635f8f000b2d3a6f4da44d69634ee5372760ccf
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-30 10:32:15 +0600

    added new api end point

--- controllers/user/index.ts
+++ controllers/user/index.ts
@@ -2,6 +2,7 @@ import catchAsyncError from "../../middleware/catchAsyncError";
 import { Request, Response, NextFunction } from "express";
 import ErrorHandler from "../../utils/errorHandler";
 import User from "../../models/user";
+import { IReqAuth, IUser } from "../../utils/interface";
 
 export const getAllUser = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -11,5 +12,16 @@ export const getAllUser = catchAsyncError(
       message: "user get successfully",
       user,
     });
+  }
+);
+
+export const getUser = catchAsyncError(
+  async (req: IReqAuth, res: Response, next: NextFunction) => {
+    if (!req.user) return;
+    const getUser = await User.findById(req.user._id);
+    if (!getUser) next(new ErrorHandler("Not Found User", 404));
+    const { password, ...otherInfo } = getUser?._doc as IUser;
+
+    res.status(404).json({ message: "User found", user: otherInfo });
   }
 );
--- routes/user/index.ts
+++ routes/user/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllUser } from "../../controllers/user";
+import { getAllUser, getUser } from "../../controllers/user";
 import { adminRole, isAuthenticated } from "../../middleware/auth";
 
 const router = Router();
 
 // When user try to register fire this function
 router.get("/users", isAuthenticated, adminRole("admin"), getAllUser);
+router.get("/user/details", isAuthenticated, getUser);
 
 export default router;
